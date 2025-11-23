652d7a1b6d83e3950d49cc4fcb9d87baec18c309
Merit <e.umurerwa@alustudent.com>
2025-11-23 22:58:51 +0200

    first app

---
+++ first_flutter_project/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Hello World",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Emerance Umurerwa",
+                style: TextStyle(fontSize: 26, color: Colors.blueGrey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
